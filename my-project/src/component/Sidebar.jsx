4b8c1c75a0aae9d99555f80fec85dde361a9ecd1
parba49 <iiuccsestudent@gmail.com>
2024-12-19 22:04:10 -0800

    sidebar updating

--- my-project/src/component/Sidebar.jsx
+++ my-project/src/component/Sidebar.jsx
@@ -6,17 +6,29 @@ const Sidebar = () => {
         <div>
             <h1 className="text-2xl font-bold">My <span className='text-red-600'>Book</span> <br /> <span className="text-4xl">Shelf</span></h1>
             <br />
-            <div className='grid grid-cols-1 gap-3'>
+           
+           <div className='grid gap-y-40'>
+           <div className='grid grid-cols-1 gap-3'>
 
                <a className='text-[#8A8A8A]' href="">Home</a>
                <input className='w-[120px]' type="text" placeholder='search'/>
                <a className='text-[#8A8A8A]' href="">MySelf</a>
                <a className='text-[#8A8A8A]' href="">Favourite</a>
 
-            
-            </div>
-  
-           
+
+           </div>
+
+
+
+
+           <div className='grid grid-cols-1 gap-3' >
+                     <a href="">About</a>
+                     <a href="">Support</a>
+                     <a href="">Term & Condition</a>
+           </div>
+
+
+           </div>
             
         </div>
     );
